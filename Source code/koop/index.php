6c781df3120e7655f913451a02d4ec7407c63ad4
Evelyn Ang <88306130+evelyn2307@users.noreply.github.com>
2024-12-31 14:46:20 +0800

    Delete Source code directory

--- Source code/koop/index.php
+++
@@ -1,6 +0,0 @@
-<!--periksa lebar skrin-->
-
-
-<script language="javascript">
-		top.location.href = "indexREDIRECT.php?width=" + screen.width; //href ke redirect & get lebar skrin
-</script>
--- Source code/koop/webdb.php
+++
@@ -1,8 +0,0 @@
-<?php //sambung "root folder" ke pangkalan data
-
-$usernameconn = "root";
-$katalaluanconn = "";
-$namadb = "dbkoop";
-$conntodb = mysqli_connect("localhost",$usernameconn,$katalaluanconn,$namadb); 
-
-?>
